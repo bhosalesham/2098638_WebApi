2aae8ccfd19bf635d1ceeed3cf17990034fdba31
bhosale.sham <bhosale.sham@gmail.com>
2023-08-16 16:32:17 +0530

    Day5_Task

---
+++ Day5/APICallFetch/APICallFetch/Controllers/ExternalDataController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace APICallFetch.Controllers
+{
+    public class ExternalDataController : Controller
+    {
+        public IActionResult UserPost() 
+        { 
+            return View(); 
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}

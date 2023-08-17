911709755a60c228c9dd88dbed761e78b68977cb
bhosale.sham <bhosale.sham@gmail.com>
2023-08-17 16:04:07 +0530

    Day 6 work shop

---
+++ Day6/ServiceAPICall/ServiceAPICall/Controllers/JSONCallController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using ServiceAPICall.Models;
+
+namespace ServiceAPICall.Controllers
+{
+    public class JSONCallController : Controller
+    {
+        CommentDetails _commentObj = new CommentDetails();
+        public IActionResult CommanDetails()
+        {
+            ViewBag.CommentObj = _commentObj.GetComments();
+            return View();
+        }
+    }
+}
